7d9ff9b49474d44b772a8e8fcfc9d9a932137025
wolfgramm1 <walkerwolfgramm@gmail.com>
2023-12-07 10:17:24 -0700

    commented out console.log on about.js/html

--- public/about.js
+++ public/about.js
@@ -9,13 +9,13 @@ function displayPicture() {
         "A Clockwork Orange", "The Green Mile", "Inception" ];
     var randomNumber = Math.floor((Math.random() * randomMovieArray.length - 1) + 1);
     var randomMovie = randomMovieArray[randomNumber];
-    console.log(randomNumber);
-    console.log(randomMovie);
+    // console.log(randomNumber);
+    // console.log(randomMovie);
   fetch(`http://www.omdbapi.com/?apikey=5966d1ce&t=`+ randomMovie)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data.Poster);
-        console.log(data.Title);
+        // console.log(data.Poster);
+        // console.log(data.Title);
     
     const containerEl = document.querySelector('#picture');
 
@@ -36,24 +36,5 @@ function displayPicture() {
     });
 }
 
-// function displayQuote(data) {
-//   fetch('https://api.quotable.io/random')
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const containerEl = document.querySelector('#quote');
-
-//       const quoteEl = document.createElement('p');
-//       quoteEl.classList.add('quote');
-//       const authorEl = document.createElement('p');
-//       authorEl.classList.add('author');
-
-//       quoteEl.textContent = data.content;
-//       authorEl.textContent = data.author;
-
-//       containerEl.appendChild(quoteEl);
-//       containerEl.appendChild(authorEl);
-//     });
-// }
-
 displayPicture();
-// displayQuote();
+
